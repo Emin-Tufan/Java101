b52a8c086fc93ed6c6fd4c371a59aae222e77001
Emin-Tufan <93204330+Emin-Tufan@users.noreply.github.com>
2022-05-16 23:51:52 +0300

    Add files via upload

--- Array2/src/Bharfi.java
+++ Array2/src/Bharfi.java
@@ -1,16 +1,21 @@
 public class Bharfi {
     public static void main(String[] args) {
 
-        String[][] dizi = new String[13][7];
+        String[][] dizi = new String[7][4];
 
         for (int i = 0; i < dizi.length; i++) {
             for (int j = 0; j < dizi[i].length; j++) {
-                if (i == 0 || j == 0 || i == 12 || i == 6 || j == 6) {
+                if (i == 0 || j == 0 || i == 3 || i == 6 || j == 3) {
                     dizi[i][j] = "*";
                 } else {
                     dizi[i][j] = " ";
                 }
+                dizi[0][3] = " ";
+                dizi[3][3] = " ";
+                dizi[6][3] = " ";
 
+
+                System.out.println();
             }
 
         }
